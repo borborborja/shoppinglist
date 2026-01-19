dac320e7fdc1274fb8319e3ec3321b6051bc4d71
Borja Balsera <borja@micapum.net>
2026-01-19 16:17:52 +0100

    chore: bump version to 1.1.0 for release

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -38,6 +38,6 @@ export default defineConfig({
     })
   ],
   define: {
-    'import.meta.env.VITE_APP_VERSION': JSON.stringify('1.0.13')
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify('1.1.0')
   }
 })
